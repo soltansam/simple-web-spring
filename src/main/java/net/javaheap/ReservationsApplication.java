29e289d5dc84648622936cf8d56683e5e96de5ee
devGuru <samsoltani.it@gmail.com>
2021-07-22 16:09:28 +0200

    initial commit

---
+++ src/main/java/net/javaheap/ReservationsApplication.java
@@ -0,0 +1,13 @@
+package net.javaheap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationsApplication.class, args);
+	}
+
+}
